fa5f5daebf84dba1d8ae0007f59a7b6464acb20e
kalpokoch <kalpokoch@gmail.com>
2025-06-12 01:15:09 +0530

    initialize

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/contact', async (req, res) => {
+  const { name, email, phone, inquiryType, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'kalpokoch@gmail.com', // this must not be empty
+    subject: `New Contact Form Submission - ${inquiryType}`,
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Phone: ${phone}
+      Inquiry Type: ${inquiryType}
+      Message: ${message}
+    `
+  };
+
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ success: true, message: 'Email sent successfully!' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: 'Failed to send email.' });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
